4ff0cd6270e802931542e3cbee61d3564c413e7d
Steve <mikomuto@users.noreply.github.com>
2021-02-23 17:32:50 -0800

    initial commit
    - just a shell of an OBSE plugin

---
+++ oblivion_multiverse/main.cpp
@@ -0,0 +1,120 @@
+#include "main.h"
+
+#if OBLIVION
+#include "obse/GameAPI.h"
+#else
+#include "obse_editor/EditorAPI.h"
+#endif
+
+IDebugLog		gLog("./Data/OBSE/Plugins/oblivion_multiverse.log");
+
+PluginHandle				g_pluginHandle = kPluginHandle_Invalid;
+
+
+
+/**********************
+* Command functions
+**********************/
+
+
+/**************************
+* Command definitions
+**************************/
+
+/*************************
+	Messaging API
+*************************/
+
+OBSEMessagingInterface* g_msg;
+
+void MessageHandler(OBSEMessagingInterface::Message* msg)
+{
+	switch (msg->type)
+	{
+	case OBSEMessagingInterface::kMessage_LoadGame:
+		_MESSAGE("OblivionMultiverse received LoadGame mesage");
+		break;
+	case OBSEMessagingInterface::kMessage_ExitGame:
+		_MESSAGE("OblivionMultiverse received ExitGame message");
+		break;
+	case OBSEMessagingInterface::kMessage_ExitToMainMenu:
+		_MESSAGE("OblivionMultiverse received ExitToMainMenu message");
+		break;
+	case OBSEMessagingInterface::kMessage_ExitGame_Console:
+		_MESSAGE("OblivionMultiverse received quit game from console message");
+		break;
+	default:
+		//_MESSAGE("OblivionMultiverse received unknown message");
+		break;
+	}
+}
+
+extern "C" {
+
+	bool OBSEPlugin_Query(const OBSEInterface* obse, PluginInfo* info)
+	{
+		_MESSAGE("OBSE queried this plugin");
+
+		// fill out the info structure
+		info->infoVersion = PluginInfo::kInfoVersion;
+		info->name = "oblivion_multiverse";
+		info->version = 1;
+
+		// version checks
+		if (!obse->isEditor)
+		{
+			if (obse->obseVersion < OBSE_VERSION_INTEGER)
+			{
+				_ERROR("OBSE version too old (got %u expected at least %u)", obse->obseVersion, OBSE_VERSION_INTEGER);
+				return false;
+			}
+
+#if OBLIVION
+			if (obse->oblivionVersion != OBLIVION_VERSION)
+			{
+				_ERROR("incorrect Oblivion version (got %08X need %08X)", obse->oblivionVersion, OBLIVION_VERSION);
+				return false;
+			}
+#endif
+
+		}
+		else
+		{
+			// no version checks needed for editor
+		}
+
+		// version checks pass
+
+		return true;
+	}
+
+	bool OBSEPlugin_Load(const OBSEInterface* obse)
+	{
+		_MESSAGE("Plugin loading");
+
+		g_pluginHandle = obse->GetPluginHandle();
+
+		// register commands
+		obse->SetOpcodeBase(0x2000); //TODO set release OpcodeBase
+
+		// register to receive messages from OBSE
+		OBSEMessagingInterface* msgIntfc = (OBSEMessagingInterface*)obse->QueryInterface(kInterface_Messaging);
+		msgIntfc->RegisterListener(g_pluginHandle, "OBSE", MessageHandler);
+		g_msg = msgIntfc;
+
+		// get command table, if needed
+		OBSECommandTableInterface* cmdIntfc = (OBSECommandTableInterface*)obse->QueryInterface(kInterface_CommandTable);
+		if (cmdIntfc) {
+#if 0	// enable the following for loads of log output
+			for (const CommandInfo* cur = cmdIntfc->Start(); cur != cmdIntfc->End(); ++cur) {
+				_MESSAGE("%s", cur->longName);
+			}
+#endif
+		}
+		else {
+			_MESSAGE("Couldn't read command table");
+		}
+		return true;
+	}
+
+};
---
+++ oblivion_multiverse/main.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "obse/PluginAPI.h"
+#include "obse/CommandTable.h"
+
+#include <string>
+
+#define MAXCLIENTS 12
+#define SUPER_VERSION 0
+#define MAIN_VERSION 0
+#define SUB_VERSION 1
+
+//externals
+extern IDebugLog gLog;
+
